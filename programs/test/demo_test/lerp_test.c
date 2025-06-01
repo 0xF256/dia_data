e23b9a658d9b7c8c82182631faac4d3d2e4231ef
SmithGoll <2022209065@qq.com>
2025-06-01 23:11:53 +0800

    fix bug in lerp test

--- programs/test/demo_test/lerp_test.c
+++ programs/test/demo_test/lerp_test.c
@@ -6,13 +6,15 @@ int main() {
     printf("Please enter startX startY endX endY frames\n");
     scanf("%d %d %d %d %d", &startX, &startY, &endX, &endY, &frames);
 
+    frames--;
+
     if (frames <= 0)
     {
         printf("Err: frames must be positive\n");
         return 1;
     }
 
-    for(int frame = 0; frame < frames; frame++)
+    for(int frame = 0; frame <= frames; frame++)
     {
         int offsetX = (endX * frame + startX * (frames - frame)) / frames;
         int offsetY = (endY * frame + startY * (frames - frame)) / frames;
