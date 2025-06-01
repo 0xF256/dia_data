5f469ede69eaa88a3083eff15d4aa1419f84dd89
SmithGoll <2022209065@qq.com>
2025-06-01 14:28:38 +0800

    add tests about demo

---
+++ programs/test/demo_test/lerp_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int startX, startY, endX, endY, frames;
+
+    printf("Please enter startX startY endX endY frames\n");
+    scanf("%d %d %d %d %d", &startX, &startY, &endX, &endY, &frames);
+
+    if (frames <= 0)
+    {
+        printf("Err: frames must be positive\n");
+        return 1;
+    }
+
+    for(int frame = 0; frame < frames; frame++)
+    {
+        int offsetX = (endX * frame + startX * (frames - frame)) / frames;
+        int offsetY = (endY * frame + startY * (frames - frame)) / frames;
+
+        printf("Frame%02d: Pos(%d, %d)\n", frame, offsetX, offsetY);
+    }
+
+    return 0;
+}
